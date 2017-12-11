7ef20458eb9bda8804686ba387016b73dfd1f4a7
RASVTECH <rasvtechnology@gmail.com>
2017-12-11 15:42:49 +0530

    Worked in addcategory.php

--- admin/addcategory.php
+++ admin/addcategory.php
@@ -4,87 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <title>Login</title>
 </head>
 <body>
 <?php
-            include "config.php";
+            include "../config.php";
             // echo "<div> <h1> Outside <h1> </div>";
             if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $cat_name=$_POST['cat_name'];
+                $cat_id=$_POST['cat_id'];
                 $submit=$_POST['submit'];
-                if(!empty($submit)) {
-                    foreach($_POST as $key=>$value) {
-                        if(empty($_POST[$key])) {
-                        $error_message = "Please enter the username and password";
-                        break;
+                if(empty($cat_name)) {
+                        $error_message = "Please add the category name";
+                    }
+                    if(isset($cat_name)&&isset($cat_id)){
+                        $checkUser_query="SELECT * FROM ekart_category WHERE cat_name='$cat_name' AND cat_id='$cat_id'";
+                        $result =mysqli_query($conn,$checkUser_query);                        
+                        if(mysqli_num_rows($result) > 0){
+                            $error_message = "Category Name,Category id  is already excist ";
                         }
                     }
-                }
                 if(!isset($error_message)){
-                  if(isset($_POST['username']) && isset($_POST['password'])){
-                    $username= $_POST['username'];
-                    $password = $_POST['password'];
-                    $selectUser_query="SELECT * FROM ekart_users WHERE username='$username' AND password='$password'";
-                    $result=mysqli_query($conn,$selectUser_query);
-                    if(mysqli_num_rows($result) > 0) {
-                      while($row=mysqli_fetch_assoc($result)){
-                        $user=$row['username'];
-                        $name=$row['first_name'];
-                        $pass=$row['password'];
-                        $id=$row['id'];
-                        $type=$row['type'];
-                      }
-                               if($username==$user && $password==$pass){
-                      session_start();
-                      $_SESSION['Firstname']=$name; 
-                      $_SESSION['username']=$user;
-                      $_SESSION['password']=$pass;
-                      $_SESSION['id']=$id;
-                      $_SESSION['type']=$type;
-                      if($type=='Admin'){
-                ?>
-                    <script>window.location.href='admin/index.php'</script>
-                <?php
-                      }else{
-                ?>
-                <script>window.location.href='index.php'</script>
-                <?php
-                      }
-                }else{
-                    $error_message="user name and password is incorrect";
+                    $insert_query= "INSERT INTO ekart_category (cat_id,cat_name) VALUES ('$cat_id','$cat_name')";
+                    $result =mysqli_query($conn,$insert_query); 
+                    if ($result === TRUE) {
+                        $success_message= "Category name inserted successfully";
+                    } else {
+                        $error_message= "Error: " . $sql . "<br>" . $conn->error;
+                    }
                 }
-                    }
-                   else{
-                      $error_message="no user";
-                    }
-              }
-            }
-          }
-              ?>
-<nav class="navbar navbar-expand-lg navbar navbar-light navbar-static-top"style="background-color: #e3f2fd;" >
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#">Profile</a>
-        </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-    </nav>
+            }  
+            ?>
+            
+            <nav class="navbar navbar-expand-lg navbar navbar-light navbar-static-top"style="background-color: #e3f2fd;" >
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Profile</a>
+                </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+            <a class="nav-link badge-light " href="logout.php">logout</a>
+            </nav>
+            <br>
+            <br>
     <div class="container">
             <div class="row">
             <div class="col-md-4">
@@ -110,11 +88,15 @@
                 <?php } ?>
                     <form method="post" action="">
                     <div class="form-group">
-                            <div>
-                                <lable for="cat_name" class="col-form-label">Category Name :</lable>
-                                <input type="text" class="form-control" name="cat_name"  />
-                            </div>
+                        <div>
+                            <lable for="cat_name" class="col-form-label">Category id :</lable>
+                            <input type="text" class="form-control" name="cat_id"  />
                         </div>
+                        <div>
+                            <lable for="cat_name" class="col-form-label">Category Name :</lable>
+                            <input type="text" class="form-control" name="cat_name"  />
+                        </div>
+                    </div>
                         <!-- <div class="form-check">
                         <lable for="addcategory" class="col-form-label">Status :</lable>
                         <label class="form-check-label">
